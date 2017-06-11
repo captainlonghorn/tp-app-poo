2d406e45ca78100e4a56e177d54b268849bf10f5
David Baudry <david.baudry@zoho.com>
2017-06-11 13:51:30 +0200

    Suppression des caches appelés dans le controller de news

--- App/Backend/Modules/Connexion/ConnexionController.php
+++ App/Backend/Modules/Connexion/ConnexionController.php
@@ -7,35 +7,31 @@ use \OCFram\HTTPRequest;
 class ConnexionController extends BackController
 {
 
-  /*
-     * La fonction demandée pour stocker les vues à cacher et leurs valeurs
-     * Le tableau est de la forme nom-de-la-vue => duree-en-secondes
-     */
-  public function createCache()
-  {
-    $this->caches = array(
-        'index' => 120,
-    );
-  }
-
-  public function executeIndex(HTTPRequest $request)
-  {
-    $this->page->addVar('title', 'Connexion');
-    
-    if ($request->postExists('login'))
+    /*
+       * La fonction demandée pour stocker les vues à cacher et leurs valeurs
+       * Le tableau est de la forme nom-de-la-vue => duree-en-secondes
+       */
+    public function createCache()
     {
-      $login = $request->postData('login');
-      $password = $request->postData('password');
-      
-      if ($login == $this->app->config()->get('login') && $password == $this->app->config()->get('pass'))
-      {
-        $this->app->user()->setAuthenticated(true);
-        $this->app->httpResponse()->redirect('.');
-      }
-      else
-      {
-        $this->app->user()->setFlash('Le pseudo ou le mot de passe est incorrect.');
-      }
+        $this->caches = array(
+            'index' => 120,
+        );
     }
-  }
+
+    public function executeIndex(HTTPRequest $request)
+    {
+        $this->page->addVar('title', 'Connexion');
+
+        if ($request->postExists('login')) {
+            $login = $request->postData('login');
+            $password = $request->postData('password');
+
+            if ($login == $this->app->config()->get('login') && $password == $this->app->config()->get('pass')) {
+                $this->app->user()->setAuthenticated(true);
+                $this->app->httpResponse()->redirect('.');
+            } else {
+                $this->app->user()->setFlash('Le pseudo ou le mot de passe est incorrect.');
+            }
+        }
+    }
 }
--- App/Backend/Modules/News/NewsController.php
+++ App/Backend/Modules/News/NewsController.php
@@ -13,154 +13,159 @@ use \OCFram\AppCacheView;
 class NewsController extends BackController
 {
 
-  /*
-  * La fonction demandée pour stocker les vues à cacher et leurs valeurs
-  * Le tableau est de la forme nom-de-la-vue => duree-en-secondes
-  */
-  public function createCache()
-  {
-    $this->caches = array(
-        'index' => 120,
-        'update' => 60,
-    );
-  }
-
-  public function executeDelete(HTTPRequest $request)
-  {
-    $newsId = $request->getData('id');
-    
-    $this->managers->getManagerOf('News')->delete($newsId);
-    $this->managers->getManagerOf('Comments')->deleteFromNews($newsId);
-
-    $this->app->user()->setFlash('La news a bien été supprimée !');
-
-    $this->app->httpResponse()->redirect('.');
-  }
-
-  public function executeDeleteComment(HTTPRequest $request)
-  {
-    $this->managers->getManagerOf('Comments')->delete($request->getData('id'));
-    
-    $this->app->user()->setFlash('Le commentaire a bien été supprimé !');
-    
-    $this->app->httpResponse()->redirect('.');
-  }
-
-  public function executeIndex(HTTPRequest $request)
-  {
-    $this->page->addVar('title', 'Gestion des news');
-
-    $manager = $this->managers->getManagerOf('News');
-
-    $this->page->addVar('listeNews', $manager->getList());
-    $this->page->addVar('nombreNews', $manager->count());
-  }
-
-  public function executeInsert(HTTPRequest $request)
-  {
-    $this->processForm($request);
-
-    $this->page->addVar('title', 'Ajout d\'une news');
-  }
-
-  public function executeUpdate(HTTPRequest $request)
-  {
-    $this->processForm($request);
-    var_dump($request->getData('id'));
-
-    $this->page->addVar('title', 'Modification d\'une news');
-
-    // Destruction du cache de la news
-    $cache = new AppCacheView('Frontend', 'News', 'show'.'-'.$request->getData('id'));
-    $cache->delete();
-    // Destruction de l'index
-    $cache = new AppCacheView('Frontend', 'News', 'index');
-    $cache->delete();
-  }
-
-  public function executeUpdateComment(HTTPRequest $request)
-  {
-    $this->page->addVar('title', 'Modification d\'un commentaire');
-
-    if ($request->method() == 'POST')
+    /*
+    * La fonction demandée pour stocker les vues à cacher et leurs valeurs
+    * Le tableau est de la forme nom-de-la-vue => duree-en-secondes
+    */
+    public function createCache()
     {
-      $comment = new Comment([
-        'id' => $request->getData('id'),
-        'auteur' => $request->postData('auteur'),
-        'contenu' => $request->postData('contenu')
-      ]);
-    }
-    else
-    {
-      $comment = $this->managers->getManagerOf('Comments')->get($request->getData('id'));
+        $this->caches = array(
+            'index' => 120,
+            'update' => 60,
+        );
     }
 
-    $formBuilder = new CommentFormBuilder($comment);
-    $formBuilder->build();
-
-    $form = $formBuilder->form();
-
-    $formHandler = new FormHandler($form, $this->managers->getManagerOf('Comments'), $request);
-
-    if ($formHandler->process())
+    public function executeDelete(HTTPRequest $request)
     {
-      $this->app->user()->setFlash('Le commentaire a bien été modifié');
+        $newsId = $request->getData('id');
 
-      $this->app->httpResponse()->redirect('/admin/');
+        $this->managers->getManagerOf('News')->delete($newsId);
+        $this->managers->getManagerOf('Comments')->deleteFromNews($newsId);
+
+        $this->app->user()->setFlash('La news a bien été supprimée !');
+
+        // Destruction du cache de la news
+        $cache = new AppCacheView('Frontend', 'News', 'show' . '-' . $request->getData('id'));
+        $cache->delete();
+        // Destruction de l'index
+        $cache = new AppCacheView('Frontend', 'News', 'index');
+        $cache->delete();
+
+        $this->app->httpResponse()->redirect('.');
     }
 
-    $this->page->addVar('form', $form->createView());
-  }
-
-  public function processForm(HTTPRequest $request)
-  {
-    if ($request->method() == 'POST')
+    public function executeDeleteComment(HTTPRequest $request)
     {
-      $news = new News([
-        'auteur' => $request->postData('auteur'),
-        'titre' => $request->postData('titre'),
-        'contenu' => $request->postData('contenu')
-      ]);
+        // retrouve la news associée au comment pour en détruire le cache
+        $newsId = $this->managers->getManagerOf('Comments')->findNewsID($request->getData('id'));
+        // delete le comment
+        $this->managers->getManagerOf('Comments')->delete($request->getData('id'));
 
-      if ($request->getExists('id'))
-      {
-        $news->setId($request->getData('id'));
-      }
-    }
-    else
-    {
-      // L'identifiant de la news est transmis si on veut la modifier
-      if ($request->getExists('id'))
-      {
-        $news = $this->managers->getManagerOf('News')->getUnique($request->getData('id'));
-      }
-      else
-      {
-        $news = new News;
-      }
+        // delete le cache de la news
+        $cache = new AppCacheView('Frontend', 'News', 'show' . '-' . $newsId);
+        $cache->delete();
+
+        $this->app->user()->setFlash('Le commentaire a bien été supprimé !');
+
+        $this->app->httpResponse()->redirect('.');
     }
 
-    $formBuilder = new NewsFormBuilder($news);
-    $formBuilder->build();
-
-    $form = $formBuilder->form();
-
-    $formHandler = new FormHandler($form, $this->managers->getManagerOf('News'), $request);
-
-    if ($formHandler->process())
+    public function executeIndex(HTTPRequest $request)
     {
-      $this->app->user()->setFlash($news->isNew() ? 'La news a bien été ajoutée !' : 'La news a bien été modifiée !');
-      
-      // Destruction du cache de la news
-      $cache = new AppCacheView('Frontend', 'News', 'show'.'-'.$request->getData('id'));
-      $cache->delete();
-      // Destruction de l'index
-      $cache = new AppCacheView('Frontend', 'News', 'index');
-      $cache->delete();
+        $this->page->addVar('title', 'Gestion des news');
 
-      $this->app->httpResponse()->redirect('/admin/');
+        $manager = $this->managers->getManagerOf('News');
+
+        $this->page->addVar('listeNews', $manager->getList());
+        $this->page->addVar('nombreNews', $manager->count());
     }
 
-    $this->page->addVar('form', $form->createView());
-  }
+    public function executeInsert(HTTPRequest $request)
+    {
+        $this->processForm($request);
+
+        $this->page->addVar('title', 'Ajout d\'une news');
+
+        // Destruction de l'index
+        $cache = new AppCacheView('Frontend', 'News', 'index');
+        $cache->delete();
+    }
+
+    public function executeUpdate(HTTPRequest $request)
+    {
+        $this->processForm($request);
+        var_dump($request->getData('id'));
+
+        $this->page->addVar('title', 'Modification d\'une news');
+
+        // Destruction du cache de la news
+        $cache = new AppCacheView('Frontend', 'News', 'show' . '-' . $request->getData('id'));
+        $cache->delete();
+        // Destruction de l'index
+        $cache = new AppCacheView('Frontend', 'News', 'index');
+        $cache->delete();
+    }
+
+    public function executeUpdateComment(HTTPRequest $request)
+    {
+        $this->page->addVar('title', 'Modification d\'un commentaire');
+
+        if ($request->method() == 'POST') {
+            $comment = new Comment([
+                'id' => $request->getData('id'),
+                'auteur' => $request->postData('auteur'),
+                'contenu' => $request->postData('contenu')
+            ]);
+        } else {
+            $comment = $this->managers->getManagerOf('Comments')->get($request->getData('id'));
+        }
+
+        $formBuilder = new CommentFormBuilder($comment);
+        $formBuilder->build();
+
+        $form = $formBuilder->form();
+
+        $formHandler = new FormHandler($form, $this->managers->getManagerOf('Comments'), $request);
+
+        if ($formHandler->process()) {
+            $this->app->user()->setFlash('Le commentaire a bien été modifié');
+
+            // Destruction du cache de la news
+            // retrouve la news associée au comment pour en détruire le cache
+            $newsId = $this->managers->getManagerOf('Comments')->findNewsID($request->getData('id'));
+            $cache = new AppCacheView('Frontend', 'News', 'show' . '-' . $newsId);
+            $cache->delete();
+
+            $this->app->httpResponse()->redirect('/admin/');
+        }
+
+        $this->page->addVar('form', $form->createView());
+    }
+
+    public function processForm(HTTPRequest $request)
+    {
+        if ($request->method() == 'POST') {
+            $news = new News([
+                'auteur' => $request->postData('auteur'),
+                'titre' => $request->postData('titre'),
+                'contenu' => $request->postData('contenu')
+            ]);
+
+            if ($request->getExists('id')) {
+                $news->setId($request->getData('id'));
+            }
+        } else {
+            // L'identifiant de la news est transmis si on veut la modifier
+            if ($request->getExists('id')) {
+                $news = $this->managers->getManagerOf('News')->getUnique($request->getData('id'));
+            } else {
+                $news = new News;
+            }
+        }
+
+        $formBuilder = new NewsFormBuilder($news);
+        $formBuilder->build();
+
+        $form = $formBuilder->form();
+
+        $formHandler = new FormHandler($form, $this->managers->getManagerOf('News'), $request);
+
+        if ($formHandler->process()) {
+            $this->app->user()->setFlash($news->isNew() ? 'La news a bien été ajoutée !' : 'La news a bien été modifiée !');
+
+            $this->app->httpResponse()->redirect('/admin/');
+        }
+
+        $this->page->addVar('form', $form->createView());
+    }
 }
